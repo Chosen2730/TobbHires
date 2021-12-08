c25ee269455ca08c52a8cd45fa6d8dbb3e5d2ba4
innocentdavid <paulinnocent04@gmail.com>
2021-12-08 09:31:56 +0100

    updated

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ $(".select-cars").hide();
 $(".sign-in-cont").hide();
 
 $(".nav").hide();
-$("#clickme").click(function(){
+$("#clickme").click(function () {
     $(".select-cars").show();
 })
-$(".button2").click(function(){
+$(".button2").click(function () {
     $(".select-cars").show();
     $('.schedule1').hide();
+    $('#orderType').val('rentalCar');
     $(".button2").css("background-color", "#113CFC");
     $(".button2").css("color", "#fff");
     $(".button1").css("background-color", "#EEEBDD");
     $(".button1").css("color", "#113CFC")
 })
-$(".button1").click(function(){
+$(".button1").click(function () {
     $(".select-cars").hide();
     $('.schedule1').show();
+    $('#orderType').val('interState');
     $(".button1").css("background-color", "#113CFC");
     $(".button1").css("color", "#fff");
     $(".button2").css("background-color", "#EEEBDD");
@@ -24,16 +26,39 @@ $(".button1").click(function(){
 $(".navbar").click(function () {
     $(".nav").slideToggle();
 })
-$(".nav-item").click(function(){
+$(".nav-item").click(function () {
     $('.nav').slideUp();
 })
-$(".sign-click").click(function(){
+$(".sign-click").click(function () {
     $('.log-in').hide();
     $(".user-img").hide();
     $('.sign-in-cont').show();
 })
-$(".login-click").click(function(){
+$(".login-click").click(function () {
     $('.log-in').show();
     $(".user-img").show();
     $('.sign-in-cont').hide();
 })
+
+$('#orderForm').submit(function (e) {
+    e.preventDefault();
+    const loading = document.querySelector('.loading');
+    loading.style.display = 'block'
+    $form = $(this)
+    var formData = new FormData(this);
+    $.ajax({
+        //  url: `http://127.0.0.1:8000/api`,
+        url: `https://tobhire.herokuapp.com/api`,
+        type: 'POST',
+        data: formData,
+        success: function (res) {
+            console.log(res)
+            alert('Your order has been shechuled successfully')
+            loading.style.display = 'none'
+            window.location = '/'
+        },
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+});
